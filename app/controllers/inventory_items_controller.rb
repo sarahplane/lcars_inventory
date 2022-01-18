c5d25cf529c0fa99ca81402db97912ef2f300c5c
Sarah Plane <sarahmplane@gmail.com>
2022-01-17 17:46:45 -0700

    Add edit and destory for inventory items

--- app/controllers/inventory_items_controller.rb
+++ app/controllers/inventory_items_controller.rb
@@ -1,4 +1,6 @@
 class InventoryItemsController < ApplicationController
+  before_action :set_inventory_item, only: [:edit, :update, :destroy]
+
   def index
     @inventory_items = InventoryItem.all
   end
@@ -17,8 +19,28 @@ class InventoryItemsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @inventory_item.update(inventory_item_params)
+      redirect_to inventory_items_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @inventory_item.destroy
+    redirect_to inventory_items_path
+  end
+
   private
     def inventory_item_params
       params.require(:inventory_item).permit(:stock_number, :status)
+    end
+
+    def set_inventory_item
+      @inventory_item = InventoryItem.find(params[:id])
     end
 end
